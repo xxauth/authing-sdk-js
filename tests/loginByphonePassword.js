443994ced70fc67991e8db6ddd4d5dc1d0c4b81e
abc <you@example.com>
2021-01-07 17:48:17 +0800

    fix

--- tests/loginByphonePassword.js
+++ tests/loginByphonePassword.js
@@ -1,13 +1,22 @@
 const auth = require('../build/main/index');
 var LocalStorage = require('node-localstorage').LocalStorage;
 global.localStorage = new LocalStorage('./scratch');
-const authenticationClient = new auth.AuthenticationClient({
-    userPoolId: "7f74f487bc121542ad0c7e3d",
-    host: "https://core.xauth.lucfish.com"
-})
+// const authenticationClient = new auth.AuthenticationClient({
+//     userPoolId: "7f74f487bc121542ad0c7e3d",
+//     host: "https://core.xauth.lucfish.com"
+// })
 
+// authenticationClient.loginByPhonePassword('15975631947', '12').then((rsp) => {
+//     console.log('rsp', rsp);
+// })
+const user = new auth.ManagementClient({
+  userPoolId: '7f74f487bc121542ad0c7e3d',
+//本地
+  secret: 'cb6254521050caf857855214bc9dba98',
+  host:'http://localhost:7001'
 
-authenticationClient.loginByPhonePassword('15975631947', '12').then((rsp) => {
-    console.log('rsp', rsp);
-})
-
+//线上
+//   secret: 'lpRzBN37G6ANWnRAz02yexQGz05wkOrr',
+//   host: 'https://console.xauth.lucfish.com'
+}).users;
+user.update('106394be-d1d1-4489-a02b-bb0bee2c1dcb', { password: '0909090' });
