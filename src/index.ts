8aa0fb561f32a2c7a63fb8f1e1c44b7a9187ec13
fioman <fioman@qq.com>
2021-03-01 23:49:09 +0800

    x

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './lib/management';
+export * from './lib/management/types';
+export * from './lib/management/ManagementTokenProvider';
 export * from './lib/auth';
+export * from './lib/auth/types';
+export * from './lib/auth/AuthenticationTokenProvider';
 export * from './types/graphql.v2';
